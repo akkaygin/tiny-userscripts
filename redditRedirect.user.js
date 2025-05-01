d555ec7828c5dddae851de6beb39ca58dc79f787
Ali Kayra Kaygın <alikayra.kaygin@hotmail.com>
2025-05-01 13:56:22 +0300

    redirect media links to actual images

--- redditRedirect.user.js
+++ redditRedirect.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Redirect New Reddit to Old Reddit
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  Redirects all reddit.com links to old.reddit.com
+// @version      0.2
+// @description  Redirects most reddit.com links to old.reddit.com
 // @author       Ali Kayra Kaygin
 // @match        *://www.reddit.com/*
 // @match        *://reddit.com/*
@@ -13,10 +13,31 @@
 (function () {
   'use strict';
 
+  let nuke = null;
+  function redirectImage() {
+    const imageParent = document.getElementsByTagName('zoomable-img')[0];
+    const imageSrc = imageParent.getElementsByTagName('img')[0].src;
+
+    document.body.innerHTML = `<div style='display:flex;justify-content:center;align-items:center;height:100vh;'><img src=${imageSrc} style='max-width:80%;max-height:80%' /></div>`;
+    document.body.style.backgroundColor = '#000';
+
+    nuke = new MutationObserver(() => {
+      const children = document.body.children;
+      for (let i = children.length - 1; i >= 1; i--) {
+        children[i].remove();
+      }
+    })
+    nuke.observe(document.body, { childList: true, subtree: true });
+  }
+
   function redirect() {
     const currentURL = window.location.href;
-    const newURL = currentURL.replace(/(www\.)?reddit\.com/, "old.reddit.com");
-    window.location.replace(newURL);
+    if (currentURL.match(/reddit\.com\/media/) == null) {
+      const newURL = currentURL.replace(/(www\.)?reddit\.com/, 'old.reddit.com');
+      window.location.replace(newURL);
+    } else {
+      redirectImage();
+    }
   }
 
   redirect();
